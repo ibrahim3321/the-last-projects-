c5db8e4296a6487fe0b173a7a285d7ac2d23068a
Omage001 <104104975+Omage001@users.noreply.github.com>
2025-10-27 18:05:51 +0300

    Update variables.tf
    dfbfgn

--- terraform/modules/aks/variables.tf
+++ terraform/modules/aks/variables.tf
@@ -2,26 +2,6 @@ variable "resource_group_name" {}
 variable "resource_group_location" {}
 variable "resource_prefix" {}
 variable "environment" {}
-
-# ACR the cluster should pull from
 variable "acr_id" {
   type = string
-}
-
-# AGIC controls
-variable "enable_agic" {
-  type    = bool
-  default = true
-}
-
-# If true, AKS will create an AppGW in a managed MC_* RG and carve a subnet for it
-variable "appgw_subnet_cidr" {
-  type    = string
-  default = "10.225.0.0/24"
-}
-
-# Name for the managed App Gateway (created by AKS when using subnet_cidr)
-variable "appgw_name" {
-  type    = string
-  default = null
 }
