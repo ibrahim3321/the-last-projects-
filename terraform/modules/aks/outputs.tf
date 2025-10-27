45169779041dc05c49f3006b866e643f0aa3f823
Omage001 <104104975+Omage001@users.noreply.github.com>
2025-10-27 08:46:30 +0300

    Update outputs.tf
    fvd

--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -7,9 +7,15 @@ output "aks_cluster_id" {
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
 }
+
 output "aks_principal_id" {
-  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
-  description = "The principal ID of the AKS managed identity for Role Assignments"
+  value       = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  description = "The principal ID of the AKS managed identity"
+}
+
+output "appgw_name" {
+  value = coalesce(var.appgw_name, "${var.resource_prefix}-appgw")
 }
